f8af29a89098658ec4adf20242eead78b271aa58
Sane404 <Sane9@i.ua>
2021-07-08 09:35:43 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import { timer, fromEvent } from "rxjs";
+import { timeInterval } from "rxjs/operators";
+const observable = timer(1, 1000);
+let subscription;
+let waitButtonSubscription;
 function App() {
+  const waitRef = useRef(); //reference for fromEvent operator
+  const [timeInSeconds, setTimeInSeconds] = useState(0);
+  const [timeString, setTimeString] = useState("");
+  const [timeElapsed, setTimeElapsed] = useState(0); //time since 0 seconds
+  const [waitWasClicked, setwaitWasClicked] = useState(false);
+  //Stop/Start button func
+
+  const timerStart_Stop = () => {
+    if (subscription?.closed || subscription === undefined) {
+      //testing if no subscription/subscription is closed
+      if (waitWasClicked) {
+        subscription = observable.subscribe((t) => {
+          setTimeInSeconds(t + timeElapsed);
+        });
+      } else {
+        subscription = observable.subscribe((t) => {
+          setTimeInSeconds(t);
+        });
+      }
+    } else {
+      //if timer is on stop and set to 0
+      setwaitWasClicked(false);
+      subscription.unsubscribe();
+      setTimeInSeconds(0);
+    }
+  };
+
+  //reset button func
+
+  const timerReset = () => {
+    subscription.unsubscribe();
+    setwaitWasClicked(false);
+    subscription = observable.subscribe((t) => {
+      setTimeInSeconds(t);
+    });
+  };
+
+  //wait button func
+
+  useEffect(() => {
+    waitButtonSubscription = fromEvent(waitRef.current, "mousedown")
+      .pipe(timeInterval())
+      .subscribe((i) => {
+        console.log(i.interval);
+        if (i.interval < 300) {
+          setTimeElapsed(timeInSeconds);
+          setwaitWasClicked(true);
+          subscription.unsubscribe();
+        }
+      });
+    return () => waitButtonSubscription.unsubscribe();
+  }, [timeInSeconds]);
+
+  //Time string update
+
+  useEffect(() => {
+    setTimeString(new Date(timeInSeconds * 1000).toISOString().substr(11, 8));
+  }, [timeInSeconds]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="clock">{timeString}</div>
+      <button onClick={timerStart_Stop} className="start">
+        Start/Stop
+      </button>
+      <button ref={waitRef} className="wait">
+        Wait
+      </button>
+      <button onClick={timerReset} className="reset">
+        Reset
+      </button>
     </div>
   );
 }
